30384645fcf8662923fe9d08e0f3429579e4dcf6
gray <gray.liang@isovalent.com>
2023-04-03 17:39:40 +0800

    handle truncate of log prefix

--- iptables-footprint.bash
+++ iptables-footprint.bash
@@ -1,28 +1,30 @@
 #! /bin/bash -e
-# iptables-footprint.bash icmp and dst host 10.1.1.1
+# IPV6=1 iptables-footprint.bash icmp and dst host 10.1.1.1
 # we may want to run "sysctl -w net.netfilter.nf_log_all_netns=1" to log all namespaces
+
+export iptables=iptables
+if [[ -n "$IPV6" ]]; then
+    export iptables=ip6tables
+fi
 
 bpf_bytecode=$(eval "nfbpf_compile RAW '$@'")
 
 function foreach_chain() {
     action=$1
-    for iptables in iptables ip6tables; do
-        for table in $($iptables-save | grep '^\*'); do
-            table=${table##\*}
-            for chain in $($iptables-save -t $table | awk '$1 ~ /^:/ {print $1}'); do
-                chain=${chain##:}
-                count=$($iptables-save -t $table | grep -c -- "-A $chain\s" || true)
-                [[ "$count" == 0 ]] && continue
-                $action $iptables $table $chain $count
-            done
+    for table in $($iptables-save | grep '^\*'); do
+        table=${table##\*}
+        for chain in $($iptables-save -t $table | awk '$1 ~ /^:/ {print $1}'); do
+            chain=${chain##:}
+            count=$($iptables-save -t $table | grep -c -- "-A $chain\s" || true)
+            [[ "$count" == 0 ]] && continue
+            $action $table $chain
         done
     done
 }
 
 function clear_log() {
-    iptables=$1
-    table=$2
-    chain=$3
+    table=$1
+    chain=$2
 
     declare -A rules
     idx=0
@@ -45,17 +47,18 @@ function clear_log() {
 }
 
 function insert_log() {
-    iptables=$1
-    table=$2
-    chain=$3
-    count=$4
+    table=$1
+    chain=$2
 
     idx=1
     while IFS= read -r line; do
-        line=${line#-A $chain }
+        line=${line%% -j *}
+        line=${line%% -g *}
+        line=${line#-A $chain } # would fail for bare '-A INPUT'
+        line=${line#-A $chain} # doesn't have side effect if the previous one succeeds
         insert_idx=$((idx*2-1))
-        echo $iptables -t $table -I $chain $insert_idx -m bpf --bytecode \"$bpf_bytecode\" ${line%%-j*} -j LOG --log-prefix "$iptables/$table/$chain/$insert_idx:"
-        eval $iptables -t $table -I $chain $insert_idx -m bpf --bytecode \"$bpf_bytecode\" ${line%%-j*} -j LOG --log-prefix "$iptables/$table/$chain/$insert_idx:"
+        echo $iptables -t $table -I $chain $insert_idx -m bpf --bytecode \"$bpf_bytecode\" $line -j LOG --log-prefix \"$insert_idx/$table/$chain\"
+        eval $iptables -t $table -I $chain $insert_idx -m bpf --bytecode \"$bpf_bytecode\" $line -j LOG --log-prefix \"$insert_idx/$table/$chain\"
         ((idx++))
     done < <($iptables-save -t $table | grep -- "^-A $chain\s")
 }
@@ -70,10 +73,12 @@ trap on_exit SIGINT
 
 echo "tail -f /var/log/syslog"
 while read line; do
-    hit=$(echo $line | grep -Po '\S+/\S+/\S+/\S+(?=:)' || true)
+    hit=$(echo $line | grep -Po '(?<= )[-\w]+/[-\w]+/[-\w]+?(?=IN=)' || true)
     [[ -z "$hit" ]] && continue
-    IFS=/ read iptables table chain idx <<<"$hit"
+    echo "->$hit"
+    IFS=/ read idx table chain <<<"$hit"
+    chain=$($iptables-save -t $table | grep -Po "(?<=-A )$chain\S*" | sort -u)
     rule=$($iptables -t $table -L $chain $((idx+1)) | tr -s ' ')
-    skb=$(echo $line | grep -Po '(?<=:)IN=.*')
+    skb=$(echo $line | grep -Po 'IN=.*')
     echo "$rule hit by $skb"
 done < <(tail -f /var/log/syslog) || true
